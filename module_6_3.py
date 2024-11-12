bcb0dff2a6537f3737975833ef78b111b2493c71
Sleeper <sleeper@sleeper.fun>
2024-11-12 16:54:15 +0300

    module_3_4.py created

---
+++ module_6_3.py
@@ -0,0 +1,69 @@
+from random import randint
+
+
+class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+
+    def __init__(self, speed):
+        self._cords = [0, 0, 0]
+        self.speed = speed
+
+    def move(self, dx, dy, dz):
+        if dz * self.speed >= 0:
+            self._cords[2] = dz * self.speed
+            self._cords[0] = dx * self.speed
+            self._cords[1] = dy * self.speed
+        else:
+            print("It's too deep, i can't dive :(")
+
+    def get_cords(self):
+        print(f'X: {self._cords[0]} Y: {self._cords[1]} Z: {self._cords[2]}')
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
+            print("Sorry, i'm peaceful :)")
+        else:
+            print("Be careful, i'm attacking you 0_0")
+
+    def speak(self):
+        print(self.sound)
+
+
+class Bird(Animal):
+    beak = True
+
+    def lay_eggs(self):
+        print(f"Here are(is) {randint(1,4)} eggs for you")
+
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+
+    def dive_in(self, dz):
+        self._cords[2] -= int(abs(dz) * self.speed / 2)
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 9
+
+class Duckbill(PoisonousAnimal, Bird, AquaticAnimal):
+    def __init__(self, speed):
+        super().__init__(speed)
+        self.sound = "Click-click-click"
+
+
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+
+db.lay_eggs()
